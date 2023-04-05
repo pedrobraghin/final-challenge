00dc693348a10c1d94ef43d08bb83aba9d392652
Pedro Braghin <braghindev@gmail.com>
2023-04-05 18:29:05 -0300

    feat: add "MongoErrors" class to improve mongo error messages

---
+++ src/utils/MongoErrors.ts
@@ -0,0 +1,32 @@
+import { MongoError } from 'mongodb';
+
+interface ResolvedError {
+  message: string;
+  statusCode: number;
+}
+
+export class MongoErrors {
+  static resolveMongoError(err: MongoError): ResolvedError {
+    // regex to filter the field name in mongo error message
+    const pattern = /{([^:]+):/i;
+    const field = err.errmsg.match(pattern);
+    let key: string = '';
+
+    if (field) {
+      key = field[1].trim();
+    }
+
+    const error: ResolvedError = {} as ResolvedError;
+
+    switch (err.code) {
+      case 11000:
+        error.message = `'${key}' already exists. Please login or create an account with other ${key}.`;
+        break;
+      default:
+        console.log(err);
+        error.message = 'Something went wrong. Please try again later.';
+        error.statusCode = 500;
+    }
+    return error;
+  }
+}
