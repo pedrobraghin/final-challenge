f781f7447b147a197f6f548edbcace74ac9c6a45
Pedro Braghin <braghindev@gmail.com>
2023-04-06 13:55:06 -0300

    fear: add "ICarsRepository" interface

---
+++ src/repositories/ICarsRepository.ts
@@ -0,0 +1,24 @@
+import { InputCarDTO } from '../interfaces/InputCarDTO';
+import { OutputCarDTO } from '../interfaces/OutputCarDTO';
+
+export interface ICarsRepository {
+  create(input: InputCarDTO): Promise<OutputCarDTO>;
+  findById(id: string, fields?: string): Promise<OutputCarDTO | null>;
+  index(
+    limit: number,
+    offset: number,
+    fields?: string
+  ): Promise<{ cars: OutputCarDTO[]; documentsCount: number }>;
+  delete(id: string): Promise<OutputCarDTO | null>;
+  update(
+    id: string,
+    input: Partial<InputCarDTO>,
+    fields?: string
+  ): Promise<OutputCarDTO | null>;
+  updateAccessory(
+    carId: string,
+    accessoryId: string,
+    input: { description: string },
+    fields?: string
+  ): Promise<OutputCarDTO | string>;
+}
