7d6e3996112fc5f643877fcd568af9018705de9f
Pedro Braghin <braghindev@gmail.com>
2023-04-05 23:28:21 -0300

    feat: add "DeleteUserByIdService" class

---
+++ src/modules/user/DeleteUserByIdService.ts
@@ -0,0 +1,24 @@
+import { InvalidParameterError } from '../../error/InvalidParameterError';
+import { NotFoundError } from '../../error/NotFoundError';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { MongoUtils } from '../../utils/MongoUtils';
+
+export class DeleteUserByIdService {
+  private usersRepository: IUsersRepository;
+
+  constructor(usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute(id: string) {
+    if (!MongoUtils.isValidId(id)) {
+      throw new InvalidParameterError('Invalid ID!');
+    }
+
+    const deletedUser = await this.usersRepository.delete(id);
+
+    if (!deletedUser) {
+      throw new NotFoundError('User not found!');
+    }
+  }
+}
