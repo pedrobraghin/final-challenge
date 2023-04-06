ad3187051fbe569e4a4b1759dfb3622079a991b3
Pedro Braghin <braghindev@gmail.com>
2023-04-06 00:36:00 -0300

    feat: add "authenticate" route to app routes

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './user.routes';
+import UsersController from '../modules/user/UsersController';
 
 const appRouter = Router();
 
+appRouter.post('/authenticate', UsersController.authenticateUser);
 appRouter.use('/user', userRouter);
 
 export { appRouter };
