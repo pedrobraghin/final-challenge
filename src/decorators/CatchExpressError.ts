06baf986856456c622a42c0d3cea499dba2581fe
Pedro Braghin <braghindev@gmail.com>
2023-04-05 21:11:55 -0300

    refactor: remove overengineering

--- src/decorators/CatchExpressError.ts
+++ src/decorators/CatchExpressError.ts
@@ -3,26 +3,20 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function CatchExpressError(
-	_target: any,
-	_methodName: string,
-	descriptor: PropertyDescriptor
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
 ) {
-	const originalMethod = descriptor.value;
+  const originalMethod = descriptor.value;
 
-	const newDescriptor: PropertyDescriptor = {
-		configurable: true,
-		get() {
-			const boundMethod = originalMethod.bind(this);
-			const catchAsyncErrors = async (
-				req: Request,
-				res: Response,
-				next: NextFunction
-			) => {
-				await boundMethod(req, res, next).catch(next);
-			};
-			return catchAsyncErrors;
-		},
-	};
+  const newDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      return async function (req: Request, res: Response, next: NextFunction) {
+        await originalMethod(req, res, next).catch(next);
+      };
+    },
+  };
 
-	return newDescriptor;
+  return newDescriptor;
 }
