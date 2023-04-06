aca022fc103127965461c14c8b8087590cead9a4
Pedro Braghin <braghindev@gmail.com>
2023-04-06 01:05:49 -0300

    feat: add "auth" middleware to authenticate user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UsersController from '../modules/user/UsersController';
+import { auth } from '../middlewares/auth';
 const userRouter = Router();
 
+userRouter.post('/', UsersController.createUser);
+
+userRouter.use(auth);
 userRouter.get('/', UsersController.getAllUsers);
 userRouter.get('/:id', UsersController.getUserById);
-userRouter.post('/', UsersController.createUser);
 userRouter.put('/');
 userRouter.delete('/:id', UsersController.deleteUserById);
 
