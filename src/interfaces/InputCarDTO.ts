bc46ef78dc3bc28abc4efd7da0f2e7bf4d9eabf8
Pedro Braghin <braghindev@gmail.com>
2023-04-06 13:54:33 -0300

    feat: add "InputCarDTO" and "OutputCarDTO" interfaces

---
+++ src/interfaces/InputCarDTO.ts
@@ -0,0 +1,10 @@
+export interface InputCarDTO {
+  model: string;
+  color: string;
+  year: number;
+  value_per_day: number;
+  accessories: Array<{
+    description: string;
+  }>;
+  number_of_passengers: number;
+}
---
+++ src/interfaces/OutputCarDTO.ts
@@ -0,0 +1,10 @@
+import { InputCarDTO } from './InputCarDTO';
+
+export interface OutputCarDTO extends InputCarDTO {
+  _id: string;
+  accessories: Array<{
+    _id: string;
+    description: string;
+  }>;
+  isReserved: boolean;
+}
