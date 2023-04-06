f8606e8c055f2b4e53f9fd0cea69a3840a0ceece
Pedro Braghin <braghindev@gmail.com>
2023-04-05 21:18:00 -0300

    feat: add 'InvalidParametersError' custom error class

---
+++ src/error/InvalidParameterError.ts
@@ -0,0 +1,8 @@
+import { BaseError } from './BaseError';
+
+export class InvalidParameterError extends BaseError {
+  constructor(message: string) {
+    super(400, message);
+    this.name = 'InvalidParameterError';
+  }
+}
