bac399c7c4c5d1988f47ddc1503cd6098ca7c1ac
Pedro Braghin <braghindev@gmail.com>
2023-04-06 00:35:09 -0300

    feat: add "AuthenticateUserService" service

---
+++ src/modules/user/AuthenticateUserService.ts
@@ -0,0 +1,34 @@
+import { InvalidParameterError } from '../../error/InvalidParameterError';
+import { UnauthorizedError } from '../../error/UnauthorizedError';
+import { JWTHandler } from '../../jwt/JWTHandler';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { PasswordUtils } from '../../utils/PasswordUtils';
+
+export class AuthenticateUserService {
+  private usersRepository: IUsersRepository;
+
+  constructor(usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute(email: string, password: string) {
+    if (!email || !password) {
+      throw new InvalidParameterError('Email and Password must be provided!');
+    }
+
+    const user = await this.usersRepository.findByEmail(email, 'password');
+
+    if (!user) {
+      throw new UnauthorizedError('Invalid email or password!');
+    }
+
+    const validPass = await PasswordUtils.comparePass(password, user.password);
+
+    if (!validPass) {
+      throw new UnauthorizedError('Invalid email or password!');
+    }
+
+    const token = JWTHandler.generateToken(user._id);
+    return token;
+  }
+}
