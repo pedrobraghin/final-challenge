3855cf2af44861160ff354110d4deecb38c113b8
Pedro Braghin <braghindev@gmail.com>
2023-04-07 21:43:33 -0300

    feat: add "GetAllCarsService" with pagination and query parameters filter

---
+++ src/modules/car/GetAllCarsService.ts
@@ -0,0 +1,38 @@
+import { InvalidParameterError } from '../../error/InvalidParameterError';
+import { ValidationError } from '../../error/ValidationError';
+import { InputCarDTO } from '../../interfaces/InputCarDTO';
+import { ICarsRepository } from '../../repositories/ICarsRepository';
+import { PaginationUtils } from '../../utils/PaginationUtils';
+import { Validators } from '../../utils/Validators';
+
+export class GetAllCarsService {
+  private carsRepository: ICarsRepository;
+
+  constructor(carsRepository: ICarsRepository) {
+    this.carsRepository = carsRepository;
+  }
+
+  async execute(
+    limit: number,
+    offset: number,
+    query: Partial<InputCarDTO> = {}
+  ) {
+    const isValidPagination = PaginationUtils.validatePagination(limit, offset);
+
+    if (!isValidPagination) {
+      throw new InvalidParameterError(
+        'Limit and offset must be numbers and greater than or equal to 0.'
+      );
+    }
+
+    if (Object.keys(query).length > 0) {
+      const queryErrors = Validators.validateQueryCar(query);
+      if (queryErrors) {
+        throw new ValidationError(queryErrors);
+      }
+    }
+
+    const cars = await this.carsRepository.index(limit, offset, query, '-__v');
+    return cars;
+  }
+}
