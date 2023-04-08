9a8091069c932bf6af74f8f90bfe321f7d0ac8d0
Pedro Braghin <braghindev@gmail.com>
2023-04-07 21:42:39 -0300

    feat: add "DeleteCarService" class

---
+++ src/modules/car/DeleteCarService.ts
@@ -0,0 +1,28 @@
+import { InvalidParameterError } from '../../error/InvalidParameterError';
+import { NotFoundError } from '../../error/NotFoundError';
+import { ICarsRepository } from '../../repositories/ICarsRepository';
+import { MongoUtils } from '../../utils/MongoUtils';
+
+export class DeleteCarService {
+  private carsRepository: ICarsRepository;
+
+  constructor(carsRepository: ICarsRepository) {
+    this.carsRepository = carsRepository;
+  }
+
+  async execute(id: string) {
+    const isValidId = MongoUtils.isValidId(id);
+
+    if (!isValidId) {
+      throw new InvalidParameterError('Invalid id!');
+    }
+
+    const deletedCar = await this.carsRepository.delete(id);
+
+    if (!deletedCar) {
+      throw new NotFoundError('Car not found!');
+    }
+
+    return deletedCar;
+  }
+}
