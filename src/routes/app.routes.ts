4670e1bdbd3acc95bb83362b460e46ac4fd97b86
Pedro Braghin <braghindev@gmail.com>
2023-04-07 22:11:02 -0300

    feat: add car routes to app router

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { userRouter } from './user.routes';
+import { carRouter } from './car.routes';
 import UsersController from '../modules/user/UsersController';
 
 const appRouter = Router();
 
 appRouter.post('/authenticate', UsersController.authenticateUser);
 appRouter.use('/user', userRouter);
+appRouter.use('/car', carRouter);
 
 export { appRouter };
