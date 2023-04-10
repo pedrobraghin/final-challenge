e40d54de54366332727973c7c8b9aa59825a0e15
Pedro Braghin <braghindev@gmail.com>
2023-04-09 23:32:02 -0300

    feat: add 'reserve' endpoint to app routes to handle with all reservation requests

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { userRouter } from './user.routes';
 import { carRouter } from './car.routes';
+import { reservationRouter } from './reservation.routes';
+
 import UsersController from '../modules/user/UsersController';
 
 const appRouter = Router();
@@ -8,5 +10,6 @@ const appRouter = Router();
 appRouter.post('/authenticate', UsersController.authenticateUser);
 appRouter.use('/user', userRouter);
 appRouter.use('/car', carRouter);
+appRouter.use('/reserve', reservationRouter);
 
 export { appRouter };
