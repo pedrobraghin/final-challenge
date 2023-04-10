649e41556cdc9b883b47712391f44dbb46b5d30b
Pedro Braghin <braghindev@gmail.com>
2023-04-10 10:48:19 -0300

    feat: add cpf validation function

--- src/utils/Validators.ts
+++ src/utils/Validators.ts
@@ -115,4 +115,60 @@ export class Validators {
     });
     return errorObj;
   }
+
+  static validateCPF(cpf: string) {
+    // remove any non-numeric characters
+    const cleanedCPF = cpf.replace(/\D+/g, '');
+
+    if (cleanedCPF.length !== 11) {
+      return false;
+    }
+
+    const invalidcleanedCPFs = [
+      '00000000000',
+      '11111111111',
+      '22222222222',
+      '33333333333',
+      '44444444444',
+      '55555555555',
+      '66666666666',
+      '77777777777',
+      '88888888888',
+      '99999999999',
+    ];
+
+    if (invalidcleanedCPFs.includes(cleanedCPF)) {
+      return false;
+    }
+
+    let sum = 0;
+    for (let i = 1; i <= 9; i++) {
+      sum += parseInt(cleanedCPF.substring(i - 1, i)) * (11 - i);
+    }
+
+    let remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cleanedCPF.substring(9, 10))) {
+      return false;
+    }
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum += parseInt(cleanedCPF.substring(i - 1, i)) * (12 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cleanedCPF.substring(10, 11))) {
+      return false;
+    }
+
+    return true;
+  }
 }
