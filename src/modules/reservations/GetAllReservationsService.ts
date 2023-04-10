9b2abe0110a9944deb37390e6cf98fde6d31013d
Pedro Braghin <braghindev@gmail.com>
2023-04-09 23:28:21 -0300

    feat: add 'GetAllReservationsService' service

---
+++ src/modules/reservations/GetAllReservationsService.ts
@@ -0,0 +1,35 @@
+import { InvalidParameterError } from '../../errors/InvalidParameterError';
+import { InputReservationDTO } from '../../interfaces/InputReservationDTO';
+import { IReservationsRepository } from '../../repositories/IReservationsRepository';
+import { PaginationUtils } from '../../utils/PaginationUtils';
+
+export class GetAllReservationsService {
+  private reservationsRepository: IReservationsRepository;
+
+  constructor(reservationsRepository: IReservationsRepository) {
+    this.reservationsRepository = reservationsRepository;
+  }
+
+  async execute(
+    limit: number,
+    offset: number,
+    query: Partial<InputReservationDTO> = {}
+  ) {
+    const isValidPagination = PaginationUtils.validatePagination(limit, offset);
+
+    if (!isValidPagination) {
+      throw new InvalidParameterError(
+        'Limit and offset must be numbers and greater than or equal to 0.'
+      );
+    }
+
+    const reservations = await this.reservationsRepository.index(
+      limit,
+      offset,
+      query,
+      '-__v'
+    );
+
+    return reservations;
+  }
+}
