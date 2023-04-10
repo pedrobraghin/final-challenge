ed2336b67a6834a7e784f10368cc4433a36f2156
Pedro Braghin <braghindev@gmail.com>
2023-04-10 18:52:31 -0300

    feat: add swagger route in app router

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import { appRouter } from './routes/app.routes';
 import { errorHandler } from './middlewares/errorHandler';
 import { notFoundRouteHandler } from './routes/notFoundRouteHandler';
+import docs from './swagger/swagger.json';
+import swaggerUi from 'swagger-ui-express';
+
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', appRouter);
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(docs));
 app.all('*', notFoundRouteHandler);
 app.use(errorHandler);
 
