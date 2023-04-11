500859f105dbfe5e142ff624d18474ceaaa46555
Pedro Braghin <braghindev@gmail.com>
2023-04-10 22:09:33 -0300

    feat: add cors, helmet and express-mongo-sanitize middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import { appRouter } from './routes/app.routes';
 import { errorHandler } from './middlewares/errorHandler';
 import { notFoundRouteHandler } from './routes/notFoundRouteHandler';
+import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 import docs from './swagger/swagger.json';
 import swaggerUi from 'swagger-ui-express';
 
 const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use(express.json());
 app.use('/api/v1', appRouter);
